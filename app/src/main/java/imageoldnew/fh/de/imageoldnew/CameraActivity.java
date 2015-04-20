f67ce2596fe861998896ddc9d83be1e4e11cf698
xFight47 <marlin.92@hotmail.de>
2015-04-20 11:43:31 +0200

    CameraActivity

--- app/src/main/java/imageoldnew/fh/de/imageoldnew/CameraActivity.java
+++ app/src/main/java/imageoldnew/fh/de/imageoldnew/CameraActivity.java
@@ -1,5 +1,7 @@
 package imageoldnew.fh.de.imageoldnew;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.hardware.Camera;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -22,20 +24,36 @@ public class CameraActivity extends ActionBarActivity {
         setContentView(R.layout.activity_camera);
 
         //Init Camera
-        try{
+        try {
             mCamera = Camera.open();//you can use open(int) to use different cameras
-        } catch (Exception e){
-            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
-        }
 
-        if(mCamera != null) {
-            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
-            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
-            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+            if (mCamera != null) {
+                mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+                FrameLayout camera_view = (FrameLayout) findViewById(R.id.camera_view);
+                camera_view.addView(mCameraView);//add the SurfaceView to the layout
+            } else {
+                throw new Exception("No camera found.");
+            }
+        } catch (Exception e) {
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+
+            //Show alert
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setMessage("Camera not working.");
+            builder1.setCancelable(true);
+            builder1.setPositiveButton("Okay",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                            System.exit(0);
+                        }
+                    });
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
         }
 
         //Button to close Application
-        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
+        ImageButton imgClose = (ImageButton) findViewById(R.id.imgClose);
         imgClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
