f337ca4e6ee6be1fdc86edcd31ee57b1e04c19bd
xFight47 <marlin.92@hotmail.de>
2015-04-28 15:43:19 +0200

    Kantenfilter

--- app/src/main/java/imageoldnew/fh/de/imageoldnew/OldNewActivity.java
+++ app/src/main/java/imageoldnew/fh/de/imageoldnew/OldNewActivity.java
@@ -2,6 +2,8 @@ package imageoldnew.fh.de.imageoldnew;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -13,6 +15,9 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
 
 
 public class OldNewActivity extends ActionBarActivity {
@@ -29,13 +34,67 @@ public class OldNewActivity extends ActionBarActivity {
         bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                imageView2.setAlpha(i/100f);
+                imageView2.setAlpha(i / 100f);
             }
+
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) { }
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) { }
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
         });
+
+
+        try {
+            InputStream inputStream = this.getResources().openRawResource(R.drawable.bahnhof);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            Bitmap bitmap = BitmapFactory.decodeStream(bufferedInputStream);
+
+            int w = bitmap.getWidth();
+            int h = bitmap.getHeight();
+            Bitmap newBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            int[][] filter = new int[][]{
+                    {0, 1, 0},
+                    {1, -4, 1},
+                    {0, 1, 0}
+            };
+            int offset = (filter.length - 1) / 2;
+            for (int y = offset; y < h - offset; y++) {
+                for (int x = offset; x < w - offset; x++) {
+                    int pixel_total = 0;
+                    int filterTotal = 0;
+                    for (int fx = 0; fx < filter.length; fx++) {
+                        for (int fy = 0; fy < filter.length; fy++) {
+
+                            int tmp_px = bitmap.getPixel(x - offset + fx, y - offset + fy);
+                            int filter_value = filter[fx][fy];
+
+                            //int a = (tmp_px >> 24 & 0xFF);
+                            int r = (tmp_px >> 16 & 0xFF);
+                            int g = (tmp_px >> 8 & 0xFF);
+                            int b = (tmp_px & 0xFF);
+
+                            pixel_total += ((r + g + b) / 3) * filter_value;
+                            filterTotal += Math.abs(filter_value);
+                        }
+                    }
+                    pixel_total = pixel_total / filterTotal;
+                    int newPx = (255 << 24) + (pixel_total << 16) + (pixel_total << 8) + pixel_total;
+                    //if (x == 0)
+                    //    Log.d("blau", "" + (newPx & 0xFFFFFF));
+                    newBitmap.setPixel(x, y, newPx);
+                }
+            }
+
+            ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
+            imageView1.setImageBitmap(newBitmap);
+
+        } catch (Exception ex) {
+            Log.e("Fehler", ex.getMessage());
+        }
+
     }
 
 
